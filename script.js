1f3c9543c55f87fa982a9b859bf274be4ea3470a
teddiebui <teddie.bbui@gmail.com>
2024-03-24 23:20:36 +0800

    new update

--- script.js
+++ script.js
@@ -9,4 +9,17 @@ $(document).ready(()=>{
             $(this).removeClass('hovered');
         }
     )
+    
+    //trang khuyen mai controller click add class
+    z = $("#khuyen-mai .button.controller")
+    z.click(function() {
+        z.removeClass("active");
+        $(this).addClass("active");
+
+        $("#khuyen-mai .promo-list > *").addClass("display-none")
+        _this = $(this)[0]
+        index = z.index(_this)
+        $($("#khuyen-mai .promo-list > *")[index]).removeClass("display-none")
+    })
+    $("#khuyen-mai .button.controller")[0].click()
 })
